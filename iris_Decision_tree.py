846cec3267b801bf4741a2089e59c2ce854e4db9
Siam Amin <34121324+siamelixir@users.noreply.github.com>
2018-10-20 14:02:04 +0600

    Update iris_Decision_tree.py

--- iris_Decision_tree.py
+++ iris_Decision_tree.py
@@ -18,10 +18,10 @@ x_train, x_test,Y_train,Y_test = train_test_split(x,Y,test_size=0.2,random_state
 
 #Function to perform training with Entropy
 clf_entropy = DecisionTreeClassifier(criterion = 'entropy', random_state=15, max_depth = 7, min_samples_leaf = 3)
-
+#training the data
 clf_entropy.fit(x_train,Y_train)
-
+#predicting the 20% of the test data
 prediction = clf_entropy.predict(x_test)
 print (("Prediction:", prediction))
-
+#acccuracy score of the test data
 print ("Accuracy : ", accuracy_score (Y_test,prediction))
